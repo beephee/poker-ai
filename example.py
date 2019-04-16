9cb90a1f2bcc5ed9936d0b2475b8e19d7c3271ec
nayirong <42468128+nayirong@users.noreply.github.com>
2019-04-16 19:31:13 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -1,14 +1,19 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import RaisedPlayer
+from Group26 import Group26Player
+from Group26_copy import Group26CopyPlayer
+from passive_player import PassivePlayer
+from honest_player import HonestPlayer
+from normal_bot import NormalBot
 
 #TODO:config the config as our wish
-config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
+config = setup_config(max_round=100, initial_stack=100000, small_blind_amount=10)
 
 
 
-config.register_player(name="f1", algorithm=RandomPlayer())
-config.register_player(name="FT2", algorithm=RaisedPlayer())
+config.register_player(name="FT1", algorithm=Group26Player())
+config.register_player(name="FT2", algorithm=Group26CopyPlayer())
 
 
 game_result = start_poker(config, verbose=1)
