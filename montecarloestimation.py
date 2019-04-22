c0c4ccee14fdae416396f1be962985705f0e035a
beephee <draciel_g@hotmail.com>
2019-04-22 15:22:45 +0800

    Final report and player upload

---
+++ montecarloestimation.py
@@ -0,0 +1,113 @@
+from pypokerengine.utils.card_utils import estimate_hole_card_win_rate, gen_cards
+#import matplotlib.pyplot as plt
+import itertools
+import random
+import operator
+import math
+
+THRESHOLD = 100
+NUM_TRIES = 1000
+
+preflop_offsuit = {"AA" : 84.93, "KK" : 82.11, "QQ" : 79.63, "JJ" : 77.15,
+                   "TT" : 74.66, "99" : 71.66, "88" : 68.71, "77" : 65.72,
+                   "AK" : 64.46, "AQ" : 63.50, "AJ" : 62.53, "66" : 62.70,
+                   "AT" : 61.56, "KQ" : 60.43, "A9" : 59.44, "KJ" : 59.44,
+                   "55" : 59.64, "A8" : 58.37, "KT" : 58.49, "A7" : 57.16,
+                   "QJ" : 56.90, "K9" : 56.40, "A6" : 55.87, "A5" : 55.74,
+                   "QT" : 55.94, "44" : 56.25, "A4" : 54.73, "K8" : 54.43,
+                   "A3" : 53.85, "Q9" : 53.86, "JT" : 53.82, "K7" : 53.41,
+                   "A2" : 52.94, "K6" : 52.29, "33" : 52.83, "Q8" : 51.93,
+                   "K5" : 51.25, "J9" : 51.63, "K4" : 50.22, "Q7" : 49.90,
+                   "T9" : 49.81, "J8" : 49.71, "K3" : 49.33, "Q6" : 48.99,
+                   "K2" : 48.42, "22" : 48.38, "Q5" : 47.95, "T8" : 47.81,
+                   "J7" : 47.72, "Q4" : 46.92, "Q3" : 46.02, "98" : 46.06,
+                   "T7" : 45.82, "J6" : 45.71, "Q2" : 45.10, "J5" : 44.90,
+                   "97" : 44.07, "J4" : 43.86, "T6" : 43.84, "J3" : 42.96,
+                   "87" : 42.69, "96" : 42.10, "J2" : 42.04, "T5" : 41.85,
+                   "T4" : 41.05, "86" : 40.69, "95" : 40.13, "T3" : 40.15,
+                   "76" : 39.95, "T2" : 39.23, "85" : 38.74, "94" : 38.08,
+                   "75" : 37.67, "93" : 37.42, "65" : 37.01, "84" : 36.70,
+                   "92" : 36.51, "74" : 35.66, "54" : 35.07, "64" : 35.00,
+                   "83" : 34.74, "82" : 34.08, "73" : 33.71, "53" : 33.16,
+                   "63" : 33.06, "43" : 32.06, "72" : 31.71, "52" : 31.19,
+                   "62" : 31.07, "42" : 30.11, "32" : 29.23}
+
+preflop_samesuit = {"AK" : 66.21, "AQ" : 65.31, "AJ" : 64.39, "AT" : 62.22,
+                    "KQ" : 62.40, "A9" : 61.50, "KJ" : 61.47, "A8" : 60.50,
+                    "KT" : 60.58, "A7" : 59.38, "QJ" : 59.07, "K9" : 58.63,
+                    "A6" : 58.17, "A5" : 58.06, "QT" : 58.17, "A4" : 57.13,
+                    "K8" : 56.79, "A3" : 56.33, "Q9" : 56.22, "K7" : 55.84,
+                    "JT" : 56.15, "A2" : 56.15, "K6" : 54.80, "Q8" : 54.41,
+                    "K5" : 53.83, "J9" : 54.11, "K4" : 52.88, "Q7" : 52.52,
+                    "K3" : 52.07, "T9" : 52.37, "J8" : 52.31, "Q6" : 51.67,
+                    "K2" : 51.23, "Q5" : 50.71, "T8" : 50.50, "J7" : 50.45,
+                    "Q4" : 49.76, "Q3" : 48.93, "98" : 48.85, "T7" : 48.65,
+                    "J6" : 48.57, "Q2" : 48.10, "J5" : 47.82, "97" : 46.99,
+                    "J4" : 46.86, "T6" : 46.80, "J3" : 46.04, "87" : 45.68,
+                    "96" : 45.15, "J2" : 45.20, "T5" : 44.93, "T4" : 44.20,
+                    "86" : 43.81, "95" : 43.31, "T3" : 43.37, "76" : 42.82,
+                    "T2" : 42.54, "85" : 41.99, "94" : 41.40, "75" : 40.97,
+                    "93" : 40.80, "65" : 40.34, "84" : 40.34, "92" : 39.97,
+                    "74" : 39.10, "54" : 38.53, "64" : 38.48, "83" : 38.28,
+                    "82" : 37.67, "73" : 37.30, "53" : 36.75, "63" : 36.68,
+                    "43" : 35.72, "72" : 35.43, "52" : 34.92, "62" : 34.83,
+                    "42" : 33.91, "32" : 33.09}
+
+# card format: [num, suit]
+outfile = 'Monte Carlo Estimation.txt'
+poss_cards = list(str(i) for i in range(2,10)) + ['T','J','Q','K','A']
+poss_suits = ['D','C','H','S']
+all_cards = [''.join(list(p)) for p in itertools.product(poss_suits, poss_cards)]
+best_nb = []
+ctr = 0
+
+for round_game in range(NUM_TRIES):
+
+    curr_cards = all_cards
+    hole_card = []
+    comm_card = []
+
+    hole_card.append(random.choice(all_cards))
+    new = random.choice(all_cards)
+    while new in hole_card:
+        new = random.choice(all_cards)
+    hole_card.append(new)
+
+    with open(outfile,'a') as writefile:
+        win_probs = []
+        x_vals = [i*1000 for i in range(1,101)]
+        for i in x_vals:
+            win_prob = []
+            for j in range(20):
+                win_prob.append(estimate_hole_card_win_rate(i, 2, gen_cards(hole_card), gen_cards(comm_card)) * 100)
+            avg = sum(win_prob)/len(win_prob)
+            if hole_card[0][0] == hole_card[1][0]:              # same suit
+                try:
+                    actual = preflop_samesuit[hole_card[0][1]+hole_card[1][1]]
+                except KeyError:
+                    actual = preflop_samesuit[hole_card[1][1]+hole_card[0][1]]
+            else:
+                try:
+                    actual = preflop_offsuit[hole_card[0][1]+hole_card[1][1]]
+                except KeyError:
+                    actual = preflop_offsuit[hole_card[1][1]+hole_card[0][1]]
+            diff = math.fabs(actual-avg)
+            if diff <= THRESHOLD:
+                writefile.write('Best number of simulations  for ' +  ','.join(hole_card) + ' is ' + str(i) + ' with deviation of ' + str(diff) + '%\n')
+                #best_nb.append(i)
+                best_nb.append(diff)
+                ctr += 1
+                print('run ' + str(ctr) + ' of ' + str(NUM_TRIES) + ' done')
+                break
+
+
+best_sim_nb = sum(best_nb)/len(best_nb)
+with open(outfile,'a') as writefile:
+    writefile.write('Best total number is ' + str(best_sim_nb) + '\n')
+    writefile.write('Max is ' + str(max(best_nb)) + '\n')
+    writefile.write('Min is ' + str(min(best_nb)) + '\n')
+    writefile.write('Range is ' + str(max(best_nb) - min(best_nb)) + '\n')
+print('Best total number is ' + str(best_sim_nb))
+print('Max is ' + str(max(best_nb)))
+print('Min is ' + str(min(best_nb)))
+print('Range is ' + str(max(best_nb) - min(best_nb)))
